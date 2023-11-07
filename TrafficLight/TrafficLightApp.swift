1c50b27af9c74ee2cd91e6870423f5fe3aa4fb2a
biibolzulpukarov <biybol07@gmail.com>
2023-11-07 15:51:09 +0100

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Бийбол Зулпукаров on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
